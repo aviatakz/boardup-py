3ae9feb764209ce6c26c84e33212c589e2959198
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2020-07-10 15:41:01 +0600

    models are fixed

--- survey/models.py
+++ survey/models.py
@@ -2,15 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Comment(models.Model):
-    content = models.TextField(default='')
+class Survey(models.Model):
+    name = models.CharField(max_length=300)
 
 
 class Interview(models.Model):
-    created_at = models.DateField()
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
-    target_user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="target_users")
-    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name="comments")
+    created_at = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
+    target_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="target_users")
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name="surveys")
+    comment = models.TextField(default='')
 
 
 class Category(models.Model):
@@ -19,17 +20,14 @@ class Category(models.Model):
 
 class Question(models.Model):
     description = models.TextField(default='')
-    created_at = models.DateField()
-    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
+    created_at = models.DateField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name="surveys")
 
 
 class Grade(models.Model):
-    created_at = models.DateField()
+    created_at = models.DateField(auto_now_add=True)
     value = models.IntegerField()
-    question_id = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="questions")
-
-
-class IntreviewGrade(models.Model):
-    interview_id = models.ForeignKey(Interview, on_delete=models.CASCADE, related_name="interviews")
-    grade_id = models.ForeignKey(Grade, on_delete=models.CASCADE, related_name="grades")
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="questions")
+    interview = models.ForeignKey(Interview, on_delete=models.CASCADE, related_name="interviews")
 

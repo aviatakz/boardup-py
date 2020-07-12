043c27a3d1beac286b882f07067f227d9de6b9c3
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2020-07-12 21:28:32 +0600

    some changes to models was done, display tables in admin page

--- survey/models.py
+++ survey/models.py
@@ -2,20 +2,31 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Survey(models.Model):
-    name = models.CharField(max_length=300)
-
-
 class Interview(models.Model):
-    created_at = models.DateField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users")
     target_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="target_users")
-    survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name="surveys")
     comment = models.TextField(default='')
+    created_at = models.DateField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = "Interview"
+        verbose_name_plural = "Interviews"
 
 
 class Category(models.Model):
     name = models.CharField(max_length=300)
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Survey(models.Model):
+    Interview = models.ForeignKey(Interview, on_delete=models.CASCADE, related_name="survey_interviews")
+
+    class Meta:
+        verbose_name = "Survey"
+        verbose_name_plural = "Surveys"
 
 
 class Question(models.Model):
@@ -24,10 +35,17 @@ class Question(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="categories")
     survey = models.ForeignKey(Survey, on_delete=models.CASCADE, related_name="surveys")
 
+    class Meta:
+        verbose_name = "Question"
+        verbose_name_plural = "Questions"
+
 
 class Grade(models.Model):
     created_at = models.DateField(auto_now_add=True)
     value = models.IntegerField()
     question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name="questions")
-    interview = models.ForeignKey(Interview, on_delete=models.CASCADE, related_name="interviews")
+    interview = models.ForeignKey(Interview, on_delete=models.CASCADE, related_name="grade_interviews")
 
+    class Meta:
+        verbose_name = "Grade"
+        verbose_name_plural = "Grades"

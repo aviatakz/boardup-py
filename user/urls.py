355fadcea1af954ad9eae7ac2419c723f74bf6bc
MagiaGroz <grozaga@gmail.com>
2020-07-16 15:00:39 +0600

    groups structure done

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import UserList, UserDetail
+from .views import UserList, UserDetail, GroupList, GroupDetail, UsersByGroupSet
 urlpatterns = [
     path('users',UserList.as_view()),
-    path(r'users/<int:pk>', UserDetail.as_view())
+    path(r'users/<int:pk>', UserDetail.as_view()),
+    path('groups', GroupList.as_view()),
+    path('groups/<int:pk>', GroupDetail.as_view()),
+    path('users/group/<int:pk>', UsersByGroupSet.as_view({'get': 'list'}))
     ]
--- user/views.py
+++ user/views.py
@@ -1,13 +1,29 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import generics
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework import generics, viewsets
 from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, GroupSerializer
 
+class UsersByGroupSet(viewsets.ViewSet):
+    def list(self, request, pk=None):
+        group = Group.objects.get(id=pk)
+        queryset = group.user_set.all()
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+class GroupList(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('name',)
+    permission_classes = (BasePermission,)
+
+class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (BasePermission,)
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()

9cbe71daf1844881cbd83cd4165592803881213a
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2020-07-17 11:37:21 +0600

    basic views were added

--- survey/serializers.py
+++ survey/serializers.py
@@ -8,19 +8,27 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
 
 
-class SurveySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Survey
-        fields = ('id', 'name')
-
-
 class QuestionSerializer(serializers.ModelSerializer):
+    category_id = serializers.IntegerField(write_only=True)
+    survey_id = serializers.IntegerField(write_only=True)
+
     class Meta:
         model = Question
         fields = ('id', 'description', 'category_id', 'survey_id', 'created_at')
 
 
+class SurveySerializer(serializers.ModelSerializer):
+    questions = QuestionSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Survey
+        fields = ('id', 'name', 'questions')
+
+
 class GradeSerializer(serializers.ModelSerializer):
+    interview_id = serializers.IntegerField(read_only=False)
+    question_id = serializers.IntegerField(read_only=False)
+
     class Meta:
         model = Grade
         fields = ('id', 'value', 'question_id', 'interview_id', 'created_at')
@@ -30,3 +38,10 @@ class InterviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Interview
         fields = ('id', 'user_id', 'target_user_id', 'survey_id', 'created_at', 'comment')
+
+
+class InterviewSurveyQuesitonSerializer(serializers.ModelSerializer):
+    survey = SurveySerializer(read_only=True)
+    class Meta:
+        model = Interview
+        fields = ('id', 'user_id', 'target_user_id', 'comment', 'survey')
--- survey/urls.py
+++ survey/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from survey import views
+
+router = DefaultRouter()
+router.register(r'questions', views.QuestionViewSet, basename='SurveyQuestions')
+router.register(r'interviews', views.InterviewViewSet, basename="UserInterviews")
+router.register(r'grades', views.GradeViewSet, basename='Grades')
+router.register(r'survey', views.SurveyViewSet, basename="Survey")
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]

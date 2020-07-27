0b84c17cd185ff81f2716bc997139c4d3ef0fccd
MagiaGroz <grozaga@gmail.com>
2020-07-27 13:38:55 +0600

    groups presentation

--- user/serializers.py
+++ user/serializers.py
@@ -3,14 +3,16 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'groups', 'photo')
-        read_only_fields = ('email',)
-
-
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('id', 'name')
+
+
+class UserSerializer(serializers.ModelSerializer):
+    groups = GroupSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'is_superuser', 'groups', 'photo')
+        read_only_fields = ('email',)

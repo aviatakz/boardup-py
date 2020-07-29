71a81a1288ff035818afcbefbc5818f5efbbbee8
MagiaGroz <grozaga@gmail.com>
2020-07-29 14:59:40 +0600

    api security

--- rating_back/settings.py
+++ rating_back/settings.py
@@ -127,6 +127,9 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
--- user/serializers.py
+++ user/serializers.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import Group
 from rest_framework import serializers
+
 from .models import User
 
 
 class GroupSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Group
         fields = ('id', 'name')
@@ -14,5 +16,5 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'email', 'is_superuser', 'groups', 'photo')
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'is_superuser', 'groups', 'photo')
         read_only_fields = ('email',)

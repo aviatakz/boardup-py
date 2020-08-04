d9006143ba2435a18cb6ce90f04083030312bc9b
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2020-08-04 13:40:31 +0600

    endpoint for creation of several interviews

--- survey/views.py
+++ survey/views.py
@@ -12,30 +12,29 @@ from survey.serializers import QuestionSerializer, InterviewSerializer, \
 class QuestionViewSet(viewsets.ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
-    #permission_classes = [IsAuthenticated] //TODO
+    # permission_classes = [IsAuthenticated] //TODO
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['survey', 'category', ]
 
     @action(detail=False, methods=['post'])
     def create_questions(self, request):
-        serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
-        if serializer.is_valid():
-            self.perform_create(serializer)
-            headers = self.get_success_headers(serializer.data)
-            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        else:
-            return Response(status=status.HTTP_400_BAD_REQUEST)
+        bulk_create(self, request)
 
 
 class InterviewViewSet(viewsets.ModelViewSet):
     queryset = Interview.objects.all()
     serializer_class = InterviewSerializer
+
     # permission_classes = [IsAuthenticated] //TODO
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = InterviewSurveyQuesitonSerializer(instance)
         return Response(serializer.data)
+
+    @action(detail=False, methods=['post'])
+    def create_interviews(self, request):
+        bulk_create(self, request)
 
     # def get_queryset(self): //TODO
     #     self.queryset = Interview.objects.filter(user=self.request.user)
@@ -62,3 +61,13 @@ class SurveyViewSet(viewsets.ModelViewSet):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+def bulk_create(self, request):
+    serializer = self.get_serializer(data=request.data, many=isinstance(request.data, list))
+    if serializer.is_valid():
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    else:
+        return Response(status=status.HTTP_400_BAD_REQUEST)

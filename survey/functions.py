096818d4de2854684bd76c57073aadc00f57178d
MagiaGroz <grozaga@gmail.com>
2020-08-05 23:46:50 +0600

    stats algorithm improvement

--- survey/functions.py
+++ survey/functions.py
@@ -1,17 +1,5 @@
-def average_list(objects):
-    map_self = {}
-    list_self = {}
-    for grade in objects:
-        category = grade.question.category.pk
-        if category in map_self:
-            map_self[category] += 1
-            list_self[category] += grade.value
-        else:
-            map_self[category] = 1
-            list_self[category] = grade.value
-
-    result = {}
-    for key, value in map_self.items():
-        result[key] = list_self[key] / value
-
-    return result
+def to_map(grades):
+    res = {}
+    for grade in grades:
+        res[grade["question__category"]] = grade["dsum"]
+    return res
--- survey/views.py
+++ survey/views.py
@@ -1,12 +1,13 @@
 import json
 
+from django.db.models import Avg
 from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from survey.functions import average_list
+from survey.functions import to_map
 from survey.models import Question, Interview, Grade, Survey, Category
 from survey.serializers import QuestionSerializer, InterviewSerializer, \
     GradeSerializer, SurveySerializer, InterviewSurveyQuesitonSerializer, CategorySerializer, SurveyQuestionsSerializer
@@ -32,7 +33,7 @@ class QuestionViewSet(viewsets.ModelViewSet):
 class InterviewViewSet(viewsets.ModelViewSet):
     queryset = Interview.objects.all()
     serializer_class = InterviewSerializer
-    
+
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
         serializer = InterviewSurveyQuesitonSerializer(instance)
@@ -44,19 +45,18 @@ class InterviewViewSet(viewsets.ModelViewSet):
         survey_id = request.data["survey_id"]
 
         grades = Grade.objects.all()
-        self_rating_grades = grades.filter(interview__survey=survey_id, interview__target_user=user_id,
-                                           interview__user=user_id)
-        self_res = average_list(self_rating_grades)
-        colleagues_rating_grades = grades.filter(interview__survey=survey_id, interview__target_user=user_id).exclude(
+        company = grades.filter(interview__survey=survey_id).values('question__category').annotate(dsum=Avg('value'))
+        colleagues = grades.filter(interview__survey=survey_id, interview__target_user=user_id).exclude(
             interview__user=user_id
-        )
-        colleagues_res = average_list(colleagues_rating_grades)
-        company_res = average_list(grades)
+        ).values('question__category').annotate(dsum=Avg('value'))
+        selff = grades.filter(interview__survey=survey_id, interview__target_user=user_id,
+                              interview__user=user_id).values('question__category').annotate(dsum=Avg('value'))
 
         categories = {category.pk: category.name for category in Category.objects.all()}
-        result_data = {"categories": categories, "self": self_res, "colleagues": colleagues_res, "company": company_res}
 
-        return HttpResponse(json.dumps(result_data, ensure_ascii=False))
+        res = {"categories": categories, "self": to_map(selff), "colleagues": to_map(colleagues),
+               "company": to_map(company)}
+        return HttpResponse(json.dumps(res, ensure_ascii=False))
 
 
 class GradeViewSet(viewsets.ModelViewSet):

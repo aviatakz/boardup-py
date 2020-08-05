db001de60ca8c86800fe3443e2e5e51f025de582
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2020-08-05 14:55:25 +0600

    fixation

--- rating_back/settings.py
+++ rating_back/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'some_default')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 INSTALLED_APPS = [
